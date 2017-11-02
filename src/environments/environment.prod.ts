b49308f387172fd70301e4288b467250d340668d
Attila Keszthelyi <syke80@gmail.com>
2017-11-02 23:34:09 +0000

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+  iconUrl: 'http://openweathermap.org/img/w/',
+  apiKey: '82082ecec131e7b4b4747e3a4dc6e98b'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
+  production: false,
   apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
   iconUrl: 'http://openweathermap.org/img/w/',
-  apiKey: '82082ecec131e7b4b4747e3a4dc6e98b',
-  production: false
+  apiKey: '82082ecec131e7b4b4747e3a4dc6e98b'
 };
